171eaf04823faf1296fdb42bb2b9fba321a68440
Taaazzz <taaazzz.bg@gmail.com>
2025-04-28 16:26:19 +0200

    Trier un tableau de chiffre ou d'object afficher dans le désordre pour les remettres en ordre croissant

--- trier_tableau_en_desordre.js
+++ trier_tableau_en_desordre.js
@@ -19,3 +19,32 @@ function trierTableau(tab) {
 
 // Exemple d'utilisation :
 console.log(trierTableau([5, 2, 9, 1, 5, 6]));
+
+
+
+//Ecrire une fonction qui trie un tableau d'objects en ordre croissant sans utiliser la méthode native .sort()
+let tableauObject = [
+    { name: "Jean", position: 10 },
+    { name: "Luc", position: 5 },
+    { name: "Lolo", position: 13 }
+];
+
+function trierTableau(tab) {
+    let results = [...tab]; // Copier le tableau original
+
+    for (let i = 0; i < results.length - 1; i++) {
+        for (let j = 0; j < results.length - 1 - i; j++) {
+            if (results[j].position > results[j + 1].position) {
+                // Échanger les deux objets
+                let temp = results[j];
+                results[j] = results[j + 1];
+                results[j + 1] = temp;
+            }
+        }
+    }
+
+    return results;
+}
+
+// Exemple d'utilisation :
+console.log(trierTableau(tableauObject));
