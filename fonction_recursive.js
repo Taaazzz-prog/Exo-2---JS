f8778f1038a153015ec4a6c23752b53eeb7a7fe2
Taaazzz <taaazzz.bg@gmail.com>
2025-05-06 09:19:26 +0200

    fonction recursive pour inverser les lettres d'un ot

--- fonction_recursive.js
+++ fonction_recursive.js
@@ -9,7 +9,7 @@ function factorielle(n){
 }
 
 // Affiche le résultat de la factorielle de 5 dans la console
-console.log(factorielle(5));
+//console.log(factorielle(5));
 
 
 // Définition de la fonction estPalindrome qui vérifie si une chaîne de caractères est un palindrome
@@ -27,4 +27,100 @@ function estPalindrome(chaine){
 }
 
 // Affiche dans la console si la chaîne "eluparcettecrapule" est un palindrome
-console.log(estPalindrome("eluparcettecrapule"));
+//console.log(estPalindrome("eluparcettecrapule"));
+
+
+
+
+//Afficher tous les elements d'un tableau en fonction recursif
+const fruits = ["pomme", "banane", "cerise"];
+function afficherTableauRecursif(tableau, index = 0) {
+    // Vérifie si l'index est inférieur à la longueur du tableau
+    if (tableau.length === index) {
+        return; // Ajout d'un retour pour arrêter la récursion
+    }
+        console.log(tableau[index]);
+        afficherTableauRecursif(tableau, index + 1);
+    }
+
+// Appelle la fonction pour afficher les éléments du tableau
+//afficherTableauRecursif(fruits);
+
+
+
+//function recursive sur un objet avec un autre objet dedans
+const personne = {
+    name: "Luc",
+    age: 30,
+    adresse: {
+        rue: "123 Rue JavaScript",
+        ville: "Nodeville",
+        country: {
+            name: "France",
+            initial: 'FR',
+            continent: {
+                name: 'Europe',
+                space: {
+                    lat: 232.312,
+                    long: 123.1,
+                    velo: {
+                        bateau: 'Reste'
+                    }
+                }
+            }
+        }
+    }
+};
+
+function afficherObjetRecursivement(object, path = '') {
+    for (let key in object) {
+        if (typeof object[key] == 'object') {
+            afficherObjetRecursivement(object[key], path? path+'.'+key : key)
+        }
+        else {
+            object[key] = String(object[key])+' salut'
+            console.log("Affiche",(path ? path+'.'+ key: key), ':', object[key])
+        }
+    }
+}
+//afficherObjetRecursivement(personne);
+       
+
+
+
+//Afficher l'arbre généalogique de manière récursive
+// Exemple d'arbre généalogique
+    const arbre = {
+        nom : "parent",
+        enfants : [
+            {
+                nom : "enfant1", enfants: []
+            },
+            {
+                nom : "enfant2", enfants: [
+                    {
+                        nom : "petit-enfant1", enfants: []
+                    }
+                ]
+            },
+            {
+                nom : "enfant3", enfants: []
+            }
+        ]
+    }
+
+// Fonction pour afficher les noms de l'arbre généalogique
+function afficherArbre(arbre, level = 0) {
+    let prefix =[];
+    for (let i = 0; i < level; i++){     
+        prefix.push('\t'); 
+       // Appel récursif pour chaque enfant
+    }  
+    console.log(prefix.join(''), 'Nom : ', arbre.nom);
+    arbre.enfants.forEach(enfant => {
+        afficherArbre(enfant, level + 1);
+    });
+}
+    afficherArbre(arbre);
+
+
